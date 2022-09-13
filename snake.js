29cbe386299d98ac48102ef3344ebf4d64cd93dc
afiz777 <6005259@mborijnland.nl>
2022-09-13 13:38:40 +0200

    snake can move to the right

--- snake.js
+++ snake.js
@@ -1,17 +1,30 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 
+
+let snakeX = 200;
+let snakeY = 200;
+
 // Draw the background
+function DrawBackground(){
 context.fillStyle = "black";
 context.fillRect(0, 0, 400, 400);
+}
 
 // Draw the snake
+function DrawSnake(){
 context.fillStyle = 'white';
-context.fillRect(200, 200, 20, 20);
-
+context.fillRect(snakeX, snakeY, 20, 20);
+}
 
 
 function update(){
-console.log('update');
+snakeX += 20;
+
+DrawBackground();
+DrawSnake();
 }
+
+DrawBackground();
+DrawSnake();
 setInterval(update,1000);
