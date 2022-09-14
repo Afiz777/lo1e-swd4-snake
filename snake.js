153e05a7b7520c6dacb03a41830925324ee7d86a
afiz777 <6005259@mborijnland.nl>
2022-09-14 12:39:37 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -5,6 +5,9 @@ let context = canvas.getContext('2d');
 let snakeX = 200;
 let snakeY = 200;
 
+
+let direction = null;
+
 // Draw the background
 function DrawBackground(){
 context.fillStyle = "black";
@@ -19,12 +22,46 @@ context.fillRect(snakeX, snakeY, 20, 20);
 
 
 function update(){
+    if(direction == 'right'){
 snakeX += 20;
-
+    } else if(direction == 'left') {
+        snakeX -= 20;
+    }
+        if(direction == 'up'){
+    snakeY -= 20;
+    } else if(direction == 'down') {
+        snakeY += 20;
+    }
 DrawBackground();
 DrawSnake();
 }
 
+
+
+
+
+function changeDirection(event) {
+
+    if( event.code == 'ArrowUp') {
+
+        direction = 'up';
+
+    } else if ( event.code == 'ArrowRight') {
+
+        direction = 'right';
+
+    } else if ( event.code == 'ArrowLeft') {
+
+        direction = 'left';
+
+    } else if ( event.code == 'ArrowDown') {
+
+        direction = 'down';
+
+    }
+}
 DrawBackground();
 DrawSnake();
-setInterval(update,1000);
+setInterval(update,500);
+addEventListener('keydown',changeDirection);
+
